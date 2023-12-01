3d19629968400cc8238236c62e815d6a9a12ab2d
Bryan Day <daytechnicalsolutions@gmail.com>
2023-11-30 22:18:29 -0800

    test t2i remove bugfix15

--- components/TextToImage.tsx
+++ components/TextToImage.tsx
@@ -175,110 +175,95 @@ export default function TextToImage() {
         <div className="font-bold text-lg py-2 my-5 font-inter border-b-2 border-gray-400 w-full">
           Modifiers
         </div>
-        <div className="mb-5">
-          <Accordion
-            collection={formData.photoArtStyle}
-            label="Photo/Art Style"
-            tooltip={
-              <Tooltip
-                info={
-                  "What kind of art style or composition should this image have?"
+
+        <div className="grid grid-cols-2 gap-3 mb-3">
+          <div className="grid rounded-lg p-4 flex-col bg-white">
+            <div className="flex gap-4 w-full">
+              <span className=" font-bold text-2xl w-full">Your Prompt: </span>
+              <div className="copy_btn" onClick={() => handleCopy(generated)}>
+                <Image
+                  src={
+                    copied === generated
+                      ? "/assets/icons/tick.svg"
+                      : "/assets/icons/copy.svg"
+                  }
+                  width={12}
+                  height={12}
+                  alt="icon"
+                />
+              </div>
+            </div>
+            <Image
+              src={`${process.env.NEXT_PUBLIC_AWS_CDN_URL}/quotes.png`}
+              width={50}
+              height={50}
+              alt="quotes"
+              className="object-fill rounded-lg max-w-sm pointer-events-none rotate-180"
+            />
+            <p className="p-2 desc mb-4 drop-shadow-lg">{displayedPrompt}</p>
+            <Image
+              src={`${process.env.NEXT_PUBLIC_AWS_CDN_URL}/quotes.png`}
+              width={50}
+              height={50}
+              alt="quotes"
+              className="object-fill rounded-lg max-w-sm pointer-events-none justify-self-end"
+            />
+          </div>
+          <div className="rounded-lg p-4 flex bg-white flex-col">
+            <div className="flex gap-4 w-full">
+              <div className="flex w-full">
+                <span className=" font-bold text-2xl">Negative Prompt </span>
+                {
+                  <Tooltip info="A negative prompt is an attempt to exclude specific things from the generated image." />
                 }
-              />
-            }
-            enumType={"PhotoArtStyle"}
-          />
-          <Accordion
-            collection={formData.realism}
-            label="Realism"
-            tooltip={<Tooltip info={"How realistic should the image be?"} />}
-            enumType={"Realism"}
-            useRadio={true}
-          />
-          <Accordion
-            collection={formData.artMedium}
-            label="Art Medium"
-            tooltip={
-              <Tooltip
-                info={"What kind of material should be used to make the image?"}
-              />
-            }
-            enumType={"ArtMedium"}
-          />
-          <Accordion
-            collection={formData.timeOfDay}
-            label="Time Of Day"
-            tooltip={
-              <Tooltip
-                info={
-                  "What time of the day should it be roughly? Typically affects environment."
-                }
-              />
-            }
-            enumType={"TimeOfDay"}
-          />
-          <Accordion
-            collection={formData.lighting}
-            label="Lighting"
-            tooltip={
-              <Tooltip
-                info={"What kind of lighting should the subject have?"}
-              />
-            }
-            enumType={"Lighting"}
-          />
-          <Accordion
-            collection={formData.colorScheme}
-            label="Color Scheme"
-            tooltip={
-              <Tooltip info={"What type of colors should the image have?"} />
-            }
-            enumType={"ColorScheme"}
-          />
-          <Accordion
-            collection={formData.mood}
-            label="Mood"
-            tooltip={
-              <Tooltip
-                info={
-                  "What kind of mood is the subject in? Typically affects faces."
-                }
-              />
-            }
-            enumType={"Mood"}
-          />
-          <Accordion
-            collection={formData.influence}
-            label="Influence"
-            tooltip={
-              <Tooltip
-                info={
-                  "Add influence of art styles from various well known artists."
-                }
-              />
-            }
-            enumType={"Influence"}
-          />
-          <Accordion
-            collection={formData.camera}
-            label="Camera"
-            tooltip={
-              <Tooltip
-                info={
-                  "What kind of camera/settings should the image be made with? May give inconsistent results based on model."
-                }
-              />
-            }
-            enumType={"Camera"}
-          />
-          <Accordion
-            collection={formData.magicWords}
-            label="Magic Words"
-            tooltip={
-              <Tooltip info={"Add various keywords to help improve quality."} />
-            }
-            enumType={"MagicWords"}
-          />
+              </div>
+              <div
+                className="copy_btn z-10 "
+                onClick={() => handleCopy(negPrompt)}
+              >
+                <Image
+                  src={
+                    copied === negPrompt
+                      ? "/assets/icons/tick.svg"
+                      : "/assets/icons/copy.svg"
+                  }
+                  width={12}
+                  height={12}
+                  alt="icon"
+                />
+              </div>
+            </div>
+            <div className="w-full gap-2 p-2">
+              <p className="desc mb-2">Sample:</p>
+              <p className=" italic">{negPrompt}</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Submit Button */}
+        <div className="flex-start mx-3 mb-5 gap-4">
+          <ButtonSubmit submitting={submitting} />
+          {session?.user &&
+            (generated === "" || generated === lorem ? (
+              <button
+                className="px-5 py-1.5 text-sm disabled:curser-default bg-white rounded-lg hover:text-white flex gap-2 flex-center hover:bg-blue-600 text-sky-500 transition-all disabled:bg-gray-500 disabled:text-gray-50"
+                disabled={true}
+              >
+                Share
+              </button>
+            ) : (
+              <Link
+                href={{
+                  pathname: "/share-prompt",
+                  query: {
+                    generatedPrompt: generated,
+                  },
+                }}
+                className="px-5 py-1.5 text-sm disabled:curser-default bg-white rounded-lg hover:text-white flex gap-2 flex-center hover:bg-blue-600 text-sky-500 transition-all"
+              >
+                Share{" "}
+              </Link>
+            ))}
         </div>
       </form>
     </section>
