744049b34050513bd9dec6bc6cda965616ad4eaf
Bryan Day <daytechnicalsolutions@gmail.com>
2023-11-30 21:50:51 -0800

    test t2i remove bugfix6

--- app/generate/text-to-image/page.tsx
+++ app/generate/text-to-image/page.tsx
@@ -1,7 +1,7 @@
 /** @format */
 "use client";
 
-import TextToImage from "@components/TextToImage";
+import { TextToImage } from "@components/TextToImage";
 import { T2IReducer } from "@utils/T2IReducer";
 import { T2IFormContext } from "@utils/utils";
 import { useReducer } from "react";
