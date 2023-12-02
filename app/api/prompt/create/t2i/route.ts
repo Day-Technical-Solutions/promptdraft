b37206a951bc5615dd4d6c6cd4cb28a9028cf7ca
Bryan Day <daytechnicalsolutions@gmail.com>
2023-12-02 03:36:33 -0800

    updated no dot from client to server 4

--- app/api/prompt/create/t2i/route.ts
+++ app/api/prompt/create/t2i/route.ts
@@ -4,11 +4,9 @@ import { T2IFormData } from "@data/Contexts";
 import { Utils } from "@utils/utils";
 
 export const POST = async (req: Request) => {
-  const formData = await req.json();
-  console.log(formData);
-  const generatedPrompt = Utils.buildPromptTextToImage(formData);
-  console.log(generatedPrompt);
+  const formData: T2IFormData = await req.json();
   try {
+    const generatedPrompt = Utils.buildPromptTextToImage(formData);
     return new Response(JSON.stringify(generatedPrompt), { status: 201 });
   } catch (error) {
     return new Response("Failed to generate prompt", { status: 500 });
