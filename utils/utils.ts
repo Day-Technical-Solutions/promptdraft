1a3138eda0bfa2975c7e10db62d2291019c29ab9
Bryan Day <daytechnicalsolutions@gmail.com>
2023-12-02 03:52:49 -0800

    updated no dot from client to server 5

--- utils/utils.ts
+++ utils/utils.ts
@@ -176,57 +176,8 @@ export class Utils {
   }
 
   public static buildPromptTextToChat(): string {
-    const prompt = `
-    Consider yourself an expert prompt engineer, someone who creates great high quality prompts for language models like yourself.
-
-    The provided JSON object represents a data structure in TypeScript for a form related to Text-to-Chat (T2C). Here's an analysis of its properties:
-    
-    thespian: Represents the character or persona involved in the conversation.
-    
-    context: Describes the contextual background or setting for the conversation.
-    
-    objective: Specifies the purpose or goal of the conversation.
-    
-    example: Potentially used to provide an illustrative instance or sample within the conversation.
-    
-    callToAction: Indicates any specific action or request made within the conversation.
-    
-    scope: Defines the extent or range covered by the conversation.
-    
-    format: Specifies the format or structure of the conversation, perhaps referring to a script or dialogue format.
-    
-    constraints: Describes any limitations or conditions applicable to the conversation.
-    
-    language: Refers to the linguistic style or choice of words in the conversation.
-    
-    keyTerms: Includes important and relevant terms or phrases in the conversation.
-    
-    tone: Represents the overall emotional or tonal quality of the conversation.
-    
-    urgency: Indicates any sense of immediacy or urgency within the conversation.
-    
-    audience: Describes the target audience or intended recipients of the conversation.
-    
-    sensitivity: Refers to the level of sensitivity or care needed in handling the conversation.
-    
-    pointOfView: Specifies the perspective or viewpoint presented in the conversation.
-    
-    visualElements: Use a prompt that ensures potentially includes descriptions of any visual elements associated with the conversation.
-    
-    counterArguments: Use a prompt that ensures addressing or including any counterarguments that might be relevant.
-    
-    analogies: Use a prompt that ensures referral to the use of analogies or comparisons within the conversation.
-    
-    quotes: Use a prompt that ensures including any quoted material or references within the conversation.
-    
-    statistics: Use a prompt that ensures the use or reference to statistical data within the conversation.
-    
-    citations: Use a prompt that specifies any sources or citations used within the conversation.
-    
-    Remember, you are taking this information and creating a prompt for a language model. If any data seems illegible, ignore it.
-
-    Use this information to create a high quality, detailed prompt that you as a language model will understand clearly. Use Markdown text formatting when responding.
-    `;
+    const prompt =
+      "Consider yourself an expert prompt engineer, someone who creates great high quality prompts for language models like yourself. The provided JSON object represents a data structure in TypeScript for a form related to Text-to-Chat (T2C) prompt generation. Here's an analysis of its properties: thespian: Represents the character or persona involved in the conversation. context: Describes the contextual background or setting for the conversation. objective: Specifies the purpose or goal of the conversation. example: Potentially used to provide an illustrative instance or sample within the conversation. callToAction: Indicates any specific action or request made within the conversation. scope: Defines the extent or range covered by the conversation. format: Specifies the format or structure of the conversation, perhaps referring to a script or dialogue format. constraints: Describes any limitations or conditions applicable to the conversation. language: Refers to the linguistic style or choice of words in the conversation. keyTerms: Includes important and relevant terms or phrases in the conversation. tone: Represents the overall emotional or tonal quality of the conversation. urgency: Indicates any sense of immediacy or urgency within the conversation. audience: Describes the target audience or intended recipients of the conversation. sensitivity: Refers to the level of sensitivity or care needed in handling the conversation. pointOfView: Specifies the perspective or viewpoint presented in the conversation. visualElements: Use a prompt that ensures potentially includes descriptions of any visual elements associated with the conversation. counterArguments: Use a prompt that ensures addressing or including any counterarguments that might be relevant. analogies: Use a prompt that ensures referral to the use of analogies or comparisons within the conversation. quotes: Use a prompt that ensures including any quoted material or references within the conversation. statistics: Use a prompt that ensures the use or reference to statistical data within the conversation. citations: Use a prompt that specifies any sources or citations used within the conversation. Remember, you are taking this information and creating a prompt for a language model. If any data seems illegible, ignore it. Use this information to create a high quality, detailed prompt that you as a language model will understand clearly. Respond with the prompt only.";
     return prompt;
   }
 }
