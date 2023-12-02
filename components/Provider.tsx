1cf6e0060b67635c26568cfcda833912764c82e0
Bryan Day <daytechnicalsolutions@gmail.com>
2023-12-02 06:05:12 -0800

    test session update 8

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -1,11 +1,16 @@
 /** @format */
 "use client";
 
-import React from "react";
-import { SessionProvider, useSession } from "next-auth/react";
+import React, { useEffect } from "react";
+import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
 
-export default function Provider({ children }: { children: React.ReactNode }) {
-  const { data: session } = useSession();
+export default function Provider({
+  children,
+  session,
+}: {
+  children: React.ReactNode;
+  session?: Session | null;
+}) {
   return <SessionProvider session={session}>{children}</SessionProvider>;
 }
