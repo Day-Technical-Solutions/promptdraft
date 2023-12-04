6e9a82ecee1b7cdc6f69e3444da513bdf61b08d8
Bryan Day <daytechnicalsolutions@gmail.com>
2023-12-04 06:15:45 -0800

    MVP ready 1.48

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -3,17 +3,14 @@
 
 import React, { useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@utils/AuthOptions";
+import { Session } from "next-auth";
 
-export default async function Provider({
+export default function Provider({
   children,
+  session,
 }: {
   children: React.ReactNode;
+  session?: Session | null;
 }) {
-  const session = await getServerSession(authOptions);
-
-  return (
-    <SessionProvider session={session ?? null}>{children}</SessionProvider>
-  );
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 }
