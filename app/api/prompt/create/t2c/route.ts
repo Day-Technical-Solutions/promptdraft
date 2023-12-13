7eec4b7dbd4cb0a137ab091d84546d789a667452
Bryan Day <daytechnicalsolutions@gmail.com>
2023-12-12 23:16:39 -0800

    Updated token limit

--- app/api/prompt/create/t2c/route.ts
+++ app/api/prompt/create/t2c/route.ts
@@ -5,25 +5,26 @@ import { Utils } from "@utils/utils";
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-  organization: "org-KPf7OJsVT5kRsafTmRQ1bC5N",
+	apiKey: process.env.OPENAI_API_KEY,
+	organization: "org-KPf7OJsVT5kRsafTmRQ1bC5N",
 });
 
 export const POST = async (req: Request) => {
-  const formData: T2CFormData = await req.json();
-  const gptPrompt = Utils.buildPromptTextToChat();
-  const completion = await openai.chat.completions.create({
-    messages: [
-      { role: "system", content: gptPrompt },
-      { role: "user", content: JSON.stringify(formData) },
-    ],
-    model: "gpt-4",
-  });
-  const generatedPrompt = completion.choices[0].message.content;
+	const formData: T2CFormData = await req.json();
+	const gptPrompt = Utils.buildPromptTextToChat();
+	const completion = await openai.chat.completions.create({
+		messages: [
+			{ role: "system", content: gptPrompt },
+			{ role: "user", content: JSON.stringify(formData) },
+		],
+		model: "gpt-4",
+		max_tokens: 5000,
+	});
+	const generatedPrompt = completion.choices[0].message.content;
 
-  try {
-    return new Response(JSON.stringify(generatedPrompt), { status: 201 });
-  } catch (error) {
-    return new Response("Failed to generate prompt", { status: 500 });
-  }
+	try {
+		return new Response(JSON.stringify(generatedPrompt), { status: 201 });
+	} catch (error) {
+		return new Response("Failed to generate prompt", { status: 500 });
+	}
 };
