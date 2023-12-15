986628f7a4545bfe8da62ba1b4eb5a449e9e56db
Bryan Day <daytechnicalsolutions@gmail.com>
2023-12-15 01:57:53 -0800

    added 10 sec revalidation

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -7,80 +7,73 @@ import { Post } from "@app/create-prompt/page";
 import { useSession } from "next-auth/react";
 
 const PromptCardList = ({
-  data,
-  handleTagClick,
+	data,
+	handleTagClick,
 }: {
-  data: Post[];
-  handleTagClick: (tag: string) => void;
+	data: Post[];
+	handleTagClick: (tag: string) => void;
 }) => {
-  return (
-    <div className="mt-16 prompt_layout h-full">
-      {data.map((post, index) => (
-        <PromptCard
-          key={index}
-          post={post}
-          handleTagClick={handleTagClick}
-          favorite={false}
-        />
-      ))}
-    </div>
-  );
+	return (
+		<div className='mt-16 prompt_layout h-full'>
+			{data.map((post, index) => (
+				<PromptCard key={index} post={post} handleTagClick={handleTagClick} favorite={false} />
+			))}
+		</div>
+	);
 };
 
 export default function Feed() {
-  const [searchText, setSearchText] = useState("");
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [filteredPosts, setFilteredPosts] = useState<Post[]>([]);
-  const { data: session } = useSession();
+	const [searchText, setSearchText] = useState("");
+	const [posts, setPosts] = useState<Post[]>([]);
+	const [filteredPosts, setFilteredPosts] = useState<Post[]>([]);
+	const { data: session } = useSession();
 
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchText(e.target.value);
-  };
+	const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchText(e.target.value);
+	};
 
-  const handleTagClick = (tag: string) => {
-    setSearchText(tag);
-  };
+	const handleTagClick = (tag: string) => {
+		setSearchText(tag);
+	};
 
-  useEffect(() => {
-    const fetchPost = async () => {
-      const response = await fetch("/api/prompt");
-      const data = await response.json();
-      setPosts(data);
-    };
-    fetchPost();
-  }, []);
+	useEffect(() => {
+		const fetchPost = async () => {
+			const response = await fetch("/api/prompt", { next: { revalidate: 10000 } });
+			const data = await response.json();
+			setPosts(data);
+		};
+		fetchPost();
+	}, []);
 
-  useEffect(() => {
-    //filter by tag, post, username
-    const filtered = posts.filter((post) => {
-      return (
-        post.creator?.userTag
-          .toLowerCase()
-          .includes(searchText.toLowerCase()) ||
-        post.prompt.toLowerCase().includes(searchText.toLowerCase()) ||
-        post.tag.toLowerCase().includes(searchText.toLowerCase())
-      );
-    });
-    setFilteredPosts(filtered);
-  }, [searchText, posts]);
+	useEffect(() => {
+		//filter by tag, post, username
+		const filtered = posts.filter((post) => {
+			return (
+				post.creator?.userTag.toLowerCase().includes(searchText.toLowerCase()) ||
+				post.prompt.toLowerCase().includes(searchText.toLowerCase()) ||
+				post.tag.toLowerCase().includes(searchText.toLowerCase())
+			);
+		});
+		setFilteredPosts(filtered);
+	}, [searchText, posts]);
 
-  return (
-    <section className="feed">
-      <form className="relative w-full flex-center">
-        <input
-          type="text"
-          placeholder="Search for a tag or a user"
-          value={searchText}
-          onChange={(e) => handleSearchChange(e)}
-          className="search_input peer"
-        />
-      </form>
-      <div className="min-h-screen">
-        <PromptCardList
-          data={searchText.length ? filteredPosts : posts}
-          handleTagClick={handleTagClick}
-        />
-      </div>
-    </section>
-  );
+	return (
+		<section className='feed'>
+			<form className='relative w-full flex-center'>
+				<input
+					type='text'
+					placeholder='Search for a tag or a user'
+					value={searchText}
+					onChange={(e) => handleSearchChange(e)}
+					className='search_input peer'
+				/>
+			</form>
+			<div className='min-h-screen'>
+				<PromptCardList
+					data={searchText.length ? filteredPosts : posts}
+					handleTagClick={handleTagClick}
+				/>
+			</div>
+		</section>
+	);
 }
