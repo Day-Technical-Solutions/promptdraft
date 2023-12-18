66f26800ba22c1bf0af9abeb9c69555fd400cbf7
Bryan Day <daytechnicalsolutions@gmail.com>
2023-12-18 09:44:50 -0800

    test ss session1

--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -3,12 +3,9 @@
 
 import React, { useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
-import { Session } from "next-auth";
+import { Session, getServerSession } from "next-auth";
 
-export default function AuthProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <SessionProvider>{children}</SessionProvider>;
+export default async function AuthProvider({ children }: { children: React.ReactNode }) {
+	const session = await getServerSession();
+	return <SessionProvider session={session}>{children}</SessionProvider>;
 }
